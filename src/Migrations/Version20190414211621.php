3c659cc67906a4c95bbd51d00b149305fae758d4
MantasAD <mantasadomav@gmail.com>
2019-04-15 00:57:01 +0300

    Fix migration

--- src/Migrations/Version20190414211621.php
+++ src/Migrations/Version20190414211621.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190414180152 extends AbstractMigration
+final class Version20190414211621 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20190414180152 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP INDEX UNIQ_8D93D64924A232CF ON user');
-        $this->addSql('ALTER TABLE user ADD surname VARCHAR(20) DEFAULT NULL, DROP sur_name');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(15) NOT NULL, surname VARCHAR(20) DEFAULT NULL, first_name VARCHAR(15) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20190414180152 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD sur_name VARCHAR(20) NOT NULL COLLATE utf8mb4_unicode_ci, DROP surname');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64924A232CF ON user (user_name)');
+        $this->addSql('DROP TABLE user');
     }
 }
