9ba4d7e778d75ad6df671e80ccad555043ca4790
MantasAD <mantasadomav@gmail.com>
2019-04-15 23:37:40 +0300

    Fix twig
    Also added redirection to index page if logged user is requesting login page

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -4,18 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class WelcomeController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(UserInterface $user = null)
+    public function index()
     {
 
-        return $this->render('index.html.twig', [
-            'user' => $user,
-        ]);
+        return $this->render('index.html.twig');
     }
 }
