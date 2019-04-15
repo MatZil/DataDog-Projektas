43bd5a5b79fde5ad31b408cdd8c5c0ff76673bdb
Matas Zilinskas <zilinskas.matas1999@gmail.com>
2019-04-16 01:49:54 +0300

    Veikianti reg forma

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,7 +16,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -33,11 +33,34 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            $message = (new \Swift_Message('My Subject'))
+                ->setFrom(['datasuniai@gmail.com' => 'Datašuniai'])
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->renderView('registration/confirmation.html.twig', [
+                        'username' => $user->getUsername()
+                    ]),
+                    'text/html'
+                );
+            $mailer->send($message);
+            //$this->CreateEvent($user->getUsername()); //This creates the event that deletes the user after 1 week. Commented out for testing convenience, if you decide to uncomment it make sure you enable confirmation e-mails in config/packages/swiftmailer.yaml
             return $this->redirectToRoute('index');
         }
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
 
+    private function CreateEvent($username){
+        $em = $this->getDoctrine()->getManager();
+
+        $RAW_QUERY = "CREATE EVENT {$username}_DELETE
+                      ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 WEEK
+                      DO
+                         DELETE FROM user
+                         WHERE username = '{$username}';";
+
+        $statement = $em->getConnection()->prepare($RAW_QUERY);
+        $statement->execute();
     }
 }
