6564849408a45f6bfa6e511771a62aa12668acad
MantasAD <mantasadomav@gmail.com>
2019-04-16 02:11:31 +0300

    Add redirection
    added redirection to index page if logged user is requesting registration page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,6 +20,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder,GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator, \Swift_Mailer $mailer): Response
     {
+        if ($this->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute("index");
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
