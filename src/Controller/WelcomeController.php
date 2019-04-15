1b5fabd5b9a48e1c2cb27919e2bcf0a761fc687c
MantasAD <mantasadomav@gmail.com>
2019-04-15 21:20:02 +0300

    Add login system

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class WelcomeController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(UserInterface $user = null)
     {
 
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'user' => $user,
+        ]);
     }
 }
