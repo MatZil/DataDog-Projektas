0faf0390b61d55bb425fc79ca8c5ddf2e3a9325d
Kontautas Kavaliauskas <kontautas5@yahoo.com>
2019-05-03 18:52:56 +0300

    nk
    nk

---
+++ ND/LoginForm.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Users: Kon
+ * Date: 3/18/2019
+ * Time: 6:32 PM
+ */
+?>
+<html>
+<head>
+
+    <title>Login Form</title>
+
+</head>
+<body>
+
+<form action="Login.php" method="post">
+    Name: <input type="text" required name="name"><br>
+    E-mail: <input type="text" required name="email"><br>
+    Password: <input type="password" required name="password"><br>
+    Submit : <input type="submit" name="button"><br>
+</form>
+
+</body>
+</html>
---
+++ ND/RegisterForm.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Users: Kon
+ * Date: 3/18/2019
+ * Time: 9:32 PM
+ */
+?>
+<html>
+<head>
+
+    <title>Register Form</title>
+
+</head>
+<body>
+
+<form action="Register.php" method="post">
+    Name: <input type="text" required name="Rname"><br>
+    E-mail: <input type="text" required name="Remail"><br>
+    Password: <input type="password" required name="Rpassword"><br>
+    Submit : <input type="submit" name="Rbutton"><br>
+</form>
+
+</body>
+</html>
