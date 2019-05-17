696b2d4ae34a764e37378cd51b054410108bc55c
Lukas <l.kezevicius@gmail.com>
2019-05-17 03:50:29 +0300

    Added tests for subscribe/unsubscribe

--- Tests/Controller/CategoryListControllerTest.php
+++ Tests/Controller/CategoryListControllerTest.php
@@ -7,8 +7,6 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use App\Form\CategoryFormType;
-
 
 class CategoryListControllerTest extends WebTestCase
 {
@@ -32,10 +30,10 @@ class CategoryListControllerTest extends WebTestCase
     {
         $client = self::createClient();
         $client->request('GET', '/categories');
-        /** Returns code 302, because user is not logged in and is redirected to index page */
+        // Returns code 302, because user is not logged in and is redirected to index page
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
 
-        /** Login admin with username: admin */
+        // Login admin with username: admin (has to be registered to the website with admin privileges)
         $client = $this->createAuthorizedClient();
         $crawler = $client->request('GET', '/categories');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
@@ -58,6 +56,7 @@ class CategoryListControllerTest extends WebTestCase
         ]);
         $client->submit($form);
 
+        // Check if user is redirected
         $this->assertTrue($client->getResponse()->isRedirect());
         $crawler = $client->followRedirect();
 
@@ -75,15 +74,13 @@ class CategoryListControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/categories');
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
-        // Edit the entity
-        //$crawler = $client->click($crawler->selectLink('Edit')->link());
-
         $form = $crawler->selectButton('Submit')->form([
             'category_form[name]' => 'TestCategoryEdited',
         ]);
 
         $client->submit($form);
 
+        // Check if user is redirected
         $this->assertTrue($client->getResponse()->isRedirect());
         $crawler = $client->followRedirect();
 
@@ -91,21 +88,45 @@ class CategoryListControllerTest extends WebTestCase
         $this->assertGreaterThan(0, $crawler->filter('html:contains("TestCategoryEdited")')->count());
     }
 
+    public function testCategorySubscribe()
+    {
+        $client = $this->createAuthorizedClient();
+        $crawler = $client->request('GET', '/categories');
+        $client->click($crawler->selectLink('Subscribe')->link());
+
+        $client->followRedirect();
+
+        // Check if the button has changed to Unsubscribe
+        $this->assertNotRegExp('/Subscribe/', $client->getResponse()->getContent());
+        $this->assertRegExp('/Unsubscribe/', $client->getResponse()->getContent());
+    }
+
+    public function testCategoryUnsubscribe()
+    {
+        $client = $this->createAuthorizedClient();
+        $crawler = $client->request('GET', '/categories');
+        $client->click($crawler->selectLink('Unsubscribe')->link());
+
+        $client->followRedirect();
+
+        // Check if the button has changed to Subscribe
+        $this->assertNotRegExp('/Unsubscribe/', $client->getResponse()->getContent());
+        $this->assertRegExp('/Subscribe/', $client->getResponse()->getContent());
+    }
+
     public function testCategoryDelete()
     {
         $client = $this->createAuthorizedClient();
         $crawler = $client->request('GET', '/categories');
-        $crawler = $client->click($crawler->selectLink('Delete')->link());
+        $client->click($crawler->selectLink('Delete')->link());
 
-        // Delete the entity
-        //$client->submit($crawler->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
         //Check data in the database
         $category = self::$kernel->getContainer()->get('doctrine')->getRepository(Category::class)->findOneByName('TestCategoryEdited');
         $this->assertEmpty($category);
 
-        // Check the entity has been delete on the list
+        // Check if the entity has been deleted from the list
         // Found exactly 1 value, because the category name is displayed in a pop up box after it is deleted
         $this->assertEquals(1, $crawler->filter('html:contains("TestCategoryEdited")')->count());
     }
