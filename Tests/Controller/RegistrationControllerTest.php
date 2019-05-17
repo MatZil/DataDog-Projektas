ae8762114c4a752995a61ed49ac2f469ff98a3ed
Lukas <l.kezevicius@gmail.com>
2019-05-17 05:04:15 +0300

    Added functional test for registration on RegistrationController

---
+++ Tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\tests\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testRegister()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/register');
+        //$crawler = $client->click($crawler->selectLink('Add category')->link());
+
+        // Fill in the form and submit it
+        $form = $crawler->selectButton('Register')->form([
+            'registration_form[email]' => 'testemail@datasuniai.com',
+            'registration_form[username]' => 'deleteme',
+            'registration_form[plainPassword][first]' => '123456',
+            'registration_form[plainPassword][second]' => '123456',
+            'registration_form[firstName]' => 'Test',
+            'registration_form[surname]' => 'Testington',
+        ]);
+        $client->submit($form);
+
+        // Check if user is redirected
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+
+        // Check data in the show view
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Registration succeed")')->count());
+
+        //Check data in the database
+        $user = self::$kernel->getContainer()->get('doctrine')->getRepository(User::class)->findOneByUsername('deleteme');
+        $this->assertEquals('deleteme', $user->getUsername());
+    }
+}
